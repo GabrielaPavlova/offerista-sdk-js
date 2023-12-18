f66e73ccb5c018cb1812550ab8fc2a47475731f7
Gabriela Pavlova <gabriela.pavlova6@abv.bg>
2023-12-18 11:08:04 +0200

    Creade JS SDK For GITHUB
    **Type:** Custom
    **Scope:** JS SDK
    **Subject:** SDK
    **Breaks:** N/A
    **Closes:** N/A
    **Details:**
    
    - Implement the logic
    - Create test page
    - Create Jest tests
    - Create documentation
    - Create README file

---
+++ v1.0.0/README.md
@@ -0,0 +1,43 @@
+# GitHub SDK and Web Application
+
+This repository contains a simple GitHub SDK and a web application that demonstrates how to authenticate with the GitHub API using a token and fetch user data based on a user ID.
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js and npm installed
+
+### Installation
+
+1. Clone the repository:
+
+git clone https://github.com/your-username/github-sdk-web-app.git
+
+### Usage
+
+1. Run the server:
+
+npm start 
+
+This will start the server at http://localhost:3000.
+
+2. Open the web application in your browser:
+http://localhost:3000
+3. Enter your GitHub API token in the "Enter GitHub API Token" input field and click the "Authenticate" button.
+4. Enter a GitHub User ID in the "Enter GitHub User ID" input field and click the "Fetch User Data" button.
+
+### SDK Documentation
+
+The SDK documentation is available in the web application at http://localhost:3000.
+
+### Test 
+
+Unit tests for the SDK functions are available in the tests directory.
+To run tests:
+
+npm test
+
+## License
+
+Only the public domain.
---
+++ v1.0.0/exampleUsage.js
@@ -0,0 +1,41 @@
+
+// Create an instance of the GitHub SDK
+const githubSDK = new GitHubSDK('https://api.github.com');
+// Function to authenticate with GitHub API token
+function authenticateWithToken() {
+  const apiTokenInput = document.getElementById('apiTokenInput').value;
+
+  try {
+    // Authenticate with the GitHub API token
+    githubSDK.authenticate(apiTokenInput);
+    console.log('Authenticated with GitHub API token.');
+  } catch (error) {
+    console.error('Authentication Error:', error.message);
+  }
+}
+
+// Function to fetch GitHub user data from the server
+async function fetchGitHubUserData() {
+    const userIdInput = document.getElementById('userIdInput').value;
+  
+    try {
+      // Fetch user data using the SDK
+      const userData = await githubSDK.getUserData(userIdInput);
+      displayUserData(userData);
+    } catch (error) {
+      console.error('Error:', error.message);
+      displayError(error.message);
+    }
+  }
+  
+  // Function to display GitHub user data on the page
+  function displayUserData(userData) {
+    const userDataContainer = document.getElementById('userDataContainer');
+    userDataContainer.innerHTML = `<pre>${JSON.stringify(userData, null, 2)}</pre>`;
+  }
+  
+  // Function to display error messages on the page
+  function displayError(message) {
+    const userDataContainer = document.getElementById('userDataContainer');
+    userDataContainer.innerHTML = `<p style="color: red;">Error: ${message}</p>`;
+  }
